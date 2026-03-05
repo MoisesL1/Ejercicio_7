0f008eb90a41f60a7176eb48337fb6455275f39d
Moises <moiseslopez1155@gmail.com>
2026-03-04 22:06:17 -0600

    Ingreso ID

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Dictionary<int, string> productos = new Dictionary<int, string>();
+
+Console.WriteLine("Ingrese la cantidad de productos que ingresaea: ");
+int cant = int.Parse(Console.ReadLine());
+
+int id;
+bool existe;
+
+for(int x = 0; x < cant; x++)
+{
+    do
+    {
+        Console.WriteLine("Ingrese el ID: ");
+        id = int.Parse(Console.ReadLine());
+
+        existe = productos.ContainsKey(id);
+
+        
+    }
+    while(!existe);
+}
