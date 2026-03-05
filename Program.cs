83b75a390198c00df610e024c40ff8d4753227a4
Moises <moiseslopez1155@gmail.com>
2026-03-04 22:14:05 -0600

    Correcciones

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿Dictionary<int, string> productos = new Dictionary<int, string>();
 
-Console.WriteLine("Ingrese la cantidad de productos que ingresaea: ");
+Console.WriteLine("Ingrese la cantidad de productos que ingresara: ");
 int cant = int.Parse(Console.ReadLine());
 
 int id;
@@ -8,25 +8,21 @@ bool existe;
 
 for(int x = 0; x < cant; x++)
 {
-    do
+    Console.WriteLine("Ingrese el ID: ");
+    id = int.Parse(Console.ReadLine());
+
+    existe = productos.ContainsKey(id);
+
+    if(existe == true)
     {
-        Console.WriteLine("Ingrese el ID: ");
-        id = int.Parse(Console.ReadLine());
-
-        existe = productos.ContainsKey(id);
-
-        if (existe == true)
-        {
-            Console.WriteLine("Ingrese el nombre del proucto: ");
-            string nombre = Console.ReadLine();
-
-            productos.Add(id, nombre);
-        }
-
-        else
-        {
-            Console.WriteLine("Ingreso un ID que ya existe. ");
-        }
+        Console.WriteLine("Ingreso un ID existente. ");
     }
-    while(!existe);
+
+    else if (existe == false)
+    {
+        Console.WriteLine("Ingrese el nombre del producto: ");
+        string nombre = Console.ReadLine();
+
+        productos.Add(id, nombre);
+    }
 }
